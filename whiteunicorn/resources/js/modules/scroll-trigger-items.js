4fa39e7b5d113f2de19d7f607f47087ed26e7af5
Bobby <bobby@whiteunicornagency.com>
2022-02-24 15:41:10 -0600

    Added examples to scrolltrigger items module

--- whiteunicorn/resources/js/modules/scroll-trigger-items.js
+++ whiteunicorn/resources/js/modules/scroll-trigger-items.js
@@ -9,7 +9,7 @@ export default function scrollTriggerAnimation() { //Use with gsap
 }
 
 /*
- * Example
+ * Examples
  *
 // With javascript
 $("h2").each(function() {
@@ -21,4 +21,52 @@ $("h2").each(function() {
 
 //Split Text Example
 $headings.attr("data-scrolltrigger", '{ "animation":"fadeIn.fromBottom", "duration":"1", "start":"top 80%", "markers": "true", "trigger":"$this", "target":".word", "stagger":".1", "splittext":"{ type:`words`,target:`$this`, wrapWords:true }" }');
+
+//On Complete Example (NOTE: This example uses `` around the data-scrolltrigger object compared to '' like the examples above. Both ways should work)
+function featuredProducts() {
+    var $sec = $main.find(".section-featured-products");
+    
+    //MOBILE
+    if (mq("(max-width: 1023px)")) {
+        $sec.find(".product-item")
+            .attr("data-scrolltrigger",`{
+                "animation": "fadeIn.fromBottom",
+                "distance": "20%",
+                "duration": "1.5",
+                "start": "top 70%",
+                "clearProps": "false",
+                "onComplete": "$this.closest('.shopify-section').addClass('animation-complete');",
+                "target": "picture.product-item__image img"
+            }`);
+    }
+    //DESKTOP
+    else {
+        $sec
+            .attr("data-scrolltrigger",`{
+                "animation": "fadeIn.fromBottom",
+                "distance": "20%",
+                "duration": "1.5",
+                "start": "top 70%",
+                "clearProps": "false",
+                "onComplete": "$this.closest('.shopify-section').addClass('animation-complete');",
+                "target": ".img-cnt",
+                "stagger": "0.15"
+            }`);
+    }
+}
+function subscribe() {
+    var $sec = $main.find(".section-subscribe");
+    
+    $sec
+    .attr("data-scrolltrigger", `{
+                "animation": "fadeIn.fromBottom",
+                "distance": "30%",
+                "duration": ".5",
+                "splittext": "{target: '.section__title', type: 'lines', lineWrap: true}",
+                "stagger": "0.15",
+                "target": ".line"
+            }`
+    );
+}
+
 */
